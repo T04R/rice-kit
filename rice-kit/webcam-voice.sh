a070115ffa44273d0f76446fb33b760f71d4846e
TR <trgit0044@gmail.com>
2026-09-14 11:52:34 +0330

    Delete rice-kit directory

--- rice-kit/webcam-voice.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if pgrep ffmpeg >/dev/null; then
-    pkill ffmpeg & echo 0 > /tmp/ffmpeg-state
-else
-    mkdir -p $HOME/Videos
-    ffmpeg -f v4l2 -i /dev/video0 -f alsa -i default -c:v libx264 -preset ultrafast -c:a aac $HOME/Videos/webcam_$(date +%Y-%m-%d-%H%M%S).mp4 & echo 1 > /tmp/ffmpeg-state
-fi
-
-
